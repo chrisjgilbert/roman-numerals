3ff0aface56929474ef627a9f2888c8fe8c02a3e
Chris Gilbert <chrisgilbert4@icloud.com>
2018-10-25 13:54:54 +0100

    passing all tests

---
+++ lib/roman_numerals.rb
@@ -0,0 +1,18 @@
+ROMAN_NUMERALS = {
+  1000 => "M", 900 => "CM", 500 => "D",
+  400 => "CD", 100 => "C",  90 => "XC",
+  50 => "L",   40 => "XL",  10 => "X",
+  9 => "IX",    5 => "V",    4 => "IV",
+  1 => "I"
+}
+
+def numeral_converter(input_number)
+  numeral_output = ""
+  ROMAN_NUMERALS.each do |numeral_key, numeral_value|
+    (input_number / numeral_key).times do
+      numeral_output << numeral_value
+      input_number -= numeral_key
+    end
+  end
+  numeral_output
+end
---
+++ spec/roman_numerals_spec.rb
@@ -0,0 +1,33 @@
+require 'roman_numerals'
+
+describe "Roman Numerals" do
+  describe "#numeral_converter" do
+    it "converts 1 to 'I'" do
+      expect(numeral_converter(1)).to eq "I"
+    end
+
+    it "converts 5 to 'V'" do
+      expect(numeral_converter(5)).to eq "V"
+    end
+
+    it "converts 10 to 'X'" do
+      expect(numeral_converter(10)).to eq "X"
+    end
+
+    it "converts 35 to 'XXV'" do
+      expect(numeral_converter(35)).to eq "XXXV"
+    end
+
+    it "converts 2001 to 'MMI'" do
+      expect(numeral_converter(2001)).to eq "MMI"
+    end
+
+    it "converts 1059 to 'MLIX'" do
+      expect(numeral_converter(1059)).to eq 'MLIX'
+    end
+
+    it "converts 1234 to 'MCCXXXIV'" do
+      expect(numeral_converter(1234)).to eq "MCCXXXIV"
+    end
+  end
+end
